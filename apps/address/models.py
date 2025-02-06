de14967f1c2f9e8c9245c4eacf7dfa1000a372b9
TranNhan2004 <trannhanlv2004@gmail.com>
2025-02-06 19:57:52 +0700

    Modify the address app 06-02-2025

--- apps/address/models.py
+++ apps/address/models.py
@@ -8,6 +8,8 @@ class Province(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=64, unique=True)
     
+    def __str__(self):
+        return self.name
     
 # -----------------------------------------------------------
 class District(models.Model):
@@ -15,9 +17,15 @@ class District(models.Model):
     name = models.CharField(max_length=64)
     province = models.ForeignKey(Province, related_name='districts', on_delete=models.CASCADE)
     
+    def __str__(self):
+        return self.name
+    
     
 # -----------------------------------------------------------
 class Commune(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=64)
     district = models.ForeignKey(District, related_name='communes', on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
