a1bab1ef4554efffb68a49fbc00dd1df23d47293
TranNhan2004 <trannhanlv2004@gmail.com>
2025-03-05 00:08:37 +0700

    Add filter for user 05-03-2025

--- apps/user_account/filters.py
+++ apps/user_account/filters.py
@@ -1,19 +1,15 @@
-from django_filters import (
-    FilterSet,
-    MultipleChoiceFilter,
-    BooleanFilter,
-    CharFilter
-)
-
+from django_filters import FilterSet, UUIDFilter, MultipleChoiceFilter
 from backend_project.choices import ROLE_CHOICES
 from .models import CustomUser
 
 
-# -----------------------------------------------------------
 class CustomUserFilter(FilterSet):
-    role_include = MultipleChoiceFilter(field_name='role', choices=ROLE_CHOICES, conjoined=False)  
+    id_not = UUIDFilter(field_name='id', method='filter_id_not')
+    role_include = MultipleChoiceFilter(field_name='role', choices=ROLE_CHOICES, conjoined=False)
     
+    def filter_id_not(self, queryset, name, value):
+        return queryset.exclude(id=value)
+
     class Meta:
         model = CustomUser
         fields = ['role_include', 'is_active']
-    
